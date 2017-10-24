c9c540db8fdc25279fea888731c99f8541f6c56d
Denis Burchenkov <d.burchenkov@newcontact.su>
2017-10-24 17:12:47 +0300

    Add js and logic

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 
 use Yii;
+use yii\web\Response;
 use yii\web\Controller;
 
 class GameController extends Controller
@@ -10,7 +11,17 @@ class GameController extends Controller
     public function actionIndex()
     {
         $grid = Yii::$app->params['grid'];
-        
+
         return $this->render('index', ['grid' => $grid]);
+    }
+
+    public function actionGetNeighbors()
+    {
+        if (Yii::$app->request->isAjax) {
+            //Yii::$app->response->format = Response::FORMAT_JSON;
+            $json = Yii::$app->request->post('id');
+            $data = json_decode($json, TRUE);
+            return $data;
+        }
     }
 }
--- views/game/index.php
+++ views/game/index.php
@@ -1,9 +1,23 @@
+<?php
+use yii\helpers\Url;
+use yii\web\View;
+
+$this->registerJsFile('@web/js/game.js', ['depends' => 'yii\web\JqueryAsset']);
+
+$options = [
+    'getNeighbors' => Url::to(['game/get-neighbors'], true)
+];
+$this->registerJs(
+    "var yiiOptions = " . \yii\helpers\Json::htmlEncode($options) . ";", View::POS_HEAD, 'yiiOptions'
+);
+
+?>
 
 <div class="container-fluid">
-    <?php for ($i = 0; $i <= $grid['widht']; $i++): ?>
+    <?php for ($i = 1; $i <= $grid['widht']; $i++): ?>
         <div class="row game">
-            <?php for ($j = 0; $j <= $grid['height']; $j++): ?>
-                <div class="col-md-1"></div>
+            <?php for ($j = 1; $j <= $grid['height']; $j++): ?>
+                <div class="col-md-1" id="<?= $i . $j ?>"></div>
             <?php endfor; ?>
         </div>
     <?php endfor; ?>
